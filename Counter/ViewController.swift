fdb099ea78cb746cd3ce99e8a992585b890769e7
Artem Chebaturov <dmachine42@gmail.com>
2023-04-27 16:59:53 +0300

    creating an counter app with text label and increase function on tap

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var counter = 0   //creating counter variable and setting its value to 0
+    
+    @IBAction func basicCounterButton(_ sender: Any) {
+        counter += 1                        //changing counter's value on +1 on every tap on a button
+        counterLabel.text = "значение счетчика: \(String(counter))" // displaying on screen text and value of a counter
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    
 
 }
 
