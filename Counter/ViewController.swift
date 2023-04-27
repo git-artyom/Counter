f0d5ce70414dc927b2e9ab75e5c04292eb3faea7
Artem Chebaturov <dmachine42@gmail.com>
2023-04-28 00:10:32 +0300

    adding plus, minus and reset to 0 buttons

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -13,10 +13,31 @@ class ViewController: UIViewController {
     
     var counter = 0   //creating counter variable and setting its value to 0
     
-    @IBAction func basicCounterButton(_ sender: Any) {
+    
+    @IBAction func plusButton(_ sender: Any) {
         counter += 1                        //changing counter's value on +1 on every tap on a button
         counterLabel.text = "значение счетчика: \(String(counter))" // displaying on screen text and value of a counter
+        
     }
+    
+    
+    @IBAction func minusButton(_ sender: Any) {
+        counter -= 1                        //changing counter's value on -1 on every tap on a button
+        if counter <= 0 { //checking for 0 and negative value
+            counter = 0 //if value <= 0 set value to 0
+        }
+        counterLabel.text = "значение счетчика: \(String(counter))" // displaying on screen text and value of a counter
+        
+    }
+    
+    @IBAction func setToNilBButton(_ sender: Any) {
+        counter = 0  //setting counter to 0
+        counterLabel.text = "значение счетчика: \(String(counter))" // displaying on screen text and value of a counter
+    }
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
